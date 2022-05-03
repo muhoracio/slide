4affc42069a6293442c47b2f829b8e3a3bbefa64
Murilo Horácio <muhoracio@gmail.com>
2022-05-03 13:36:10 -0300

    Calculo de dsitâncias quando os eventos ocorrem

--- js/slide.js
+++ js/slide.js
@@ -2,18 +2,33 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPos: 0, startX: 0, movement: 0 };
+  }
+
+  moveSlide(distX) {
+    this.dist.movePos = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0px, 0px)`;
+  }
+  
+  updatePos(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPos - this.dist.movement;
   }
 
   onStart(e) {
     e.preventDefault();
+    this.dist.startX = e.clientX;
     this.wrapper.addEventListener("mousemove", this.onMove);
   }
   
   onMove(e) {
+    const finalPos = this.updatePos(e.clientX);
+    this.moveSlide(finalPos);
   }
   
-  onEnd(e) {
+  onEnd() {
     this.wrapper.removeEventListener("mousemove", this.onMove);
+    this.dist.finalPos = this.dist.movePos;
   }
 
   addSlideEvents() {
